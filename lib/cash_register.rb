6b5f6eabc748a7de45fe1ed9b806ef31c658f388
Isaac <deisaacalejandro@gmail.com>
2020-11-30 02:02:39 -0500

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,35 @@
 
+class CashRegister
+
+    def initialize(discount = 0)
+        @discount = discount
+        @total = 0
+    end
+
+    def total=(total)
+        @total = total 
+    end
+
+    def total
+        return @total
+    end
+
+    def add_item(item, price, ammount)
+        self.total += price
+        if ammount > 0
+            ammount *= price 
+        end
+
+            
+    end
+
+    def discount=(discount)
+        @discount = discount
+    end
+
+    def discount
+        @discount
+    end
+
+
+end
